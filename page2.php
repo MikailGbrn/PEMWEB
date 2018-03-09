d0b355edffdf94935d2ce646b77f7b92a1ba00fe
bungaasmara <asmarabungaa@yahoo.com>
2018-03-09 22:44:03 +0700

    FORM
    FORM PHP

---
+++ page2.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1"/>
+  <title>E-Banking Bunga Next</title>
+
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.min.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  
+</head>
+<body style="background-image: url(header.jpeg); background-attachment: fixed;">
+<div class="container">
+	<div class="section">
+		<div class="row">
+			<div class="col s12">
+				<div class="card">
+					<div class="row">
+						<div class="col s12">
+							<h4 class="center">Data Pelanggan 260298</h4>
+							<table style="margin-left: 20px;">
+								<tr>
+									<td style="width: 200px;">Nama</td>
+									<td>:</td>
+									<td>Bunga Asmara</td>
+								</tr>
+								<tr>
+									<td>Email</td>
+									<td>:</td>
+									<td>bungaasmaraa@gmail.com</td>
+								</tr>
+								<tr>
+									<td>Username</td>
+									<td>:</td>
+									<td>bungaasmara</td>
+								</tr>
+								<tr>
+									<td>Pekerjaan</td>
+									<td>:</td>
+									<td>Mahasiswa</td>
+								</tr>
+								<tr>
+									<td>Paket Yang dipilih</td>
+									<td>:</td>
+									<td>Deposito, Tabungan</td>
+								</tr>
+								<tr>
+									<td>Aktivasi Layanan</td>
+									<td>:</td>
+									<td>Mobile Banking</td>
+								</tr>
+								<tr>
+									<td>Layanan Tambahan</td>
+									<td>:</td>
+									<td>Kartu ATM</td>
+								</tr>
+								<tr>
+									<td>Jenis Member</td>
+									<td>:</td>
+									<td>Silver</td>
+								</tr>
+								<tr>
+									<td>Keterangan Tambahan</td>
+									<td>:</td>
+									<td>-</td>
+								</tr>
+								<tr>
+									<td>Scan KTP</td>
+									<td>:</td>
+									<td><img src="EKTP.jpg" alt="" width="100px"></td>
+								</tr>
+
+							</table>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+  
+
+  <!--  Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="js/materialize.min.js"></script>
+  <script src="js/init.js"></script>
+
+</body>
+</html>
